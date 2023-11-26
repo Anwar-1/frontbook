605ef046cce49ff17f7ac73b12467838782d1315
Anwar-1 <77014464+Anwar-1@users.noreply.github.com>
2023-11-26 22:20:51 +0300

    Update createBook.jsx

--- src/routes/createBook.jsx
+++ src/routes/createBook.jsx
@@ -26,7 +26,7 @@ function createBook() {
 
     try {
 
-      const response = await fetch("http://localhost:8000/api/books", {
+      const response = await fetch("https://bookback-eight.vercel.app/api/books", {
         method: "POST",
         body: formData,
       });
